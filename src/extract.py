64aef3b15c9bc2135a98db180f44cc76ce6d11f8
Alejandro <aletomasperren@gmail.com>
2026-01-30 17:56:28 -0300

    feat(etl): implement extract step

--- src/extract.py
+++ src/extract.py
@@ -1,3 +1,16 @@
 import pandas as pd
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+RAW_DIR = BASE_DIR / "data"
 
+def extract_data():
+    df_full_products = pd.read_csv(RAW_DIR / "productsfull.csv")
+    df_full_products2 = pd.read_csv(RAW_DIR / "productsfull2.csv")
+    df_products_classifield = pd.read_csv(RAW_DIR / "productsclassified.csv")
+
+    return (
+        df_full_products,
+        df_full_products2,
+        df_products_classifield
+    )
